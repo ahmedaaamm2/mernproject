b1fc9113ecd4b0668e5e1de0e40b178ac702726c
ahmed <ahmedaaamm2gmail.com>
2024-10-07 17:14:05 +0300

    aa

---
+++ server/models/Users.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+UserSchema = new mongoose.Schema({
+    name:{type:String},
+    age:{type:Number},
+    email:{type:String},
+})
+
+const UserModel = mongoose.model("Users", UserSchema)
+
+module.exports = UserModel 
